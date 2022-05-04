aea65a97d893dc5b16b98d06dda099f1b8d9552c
Selim Fazylov <selim.fazylov1@gmail.com>
2022-05-04 15:27:17 +0300

    toolkit inst

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,8 +2,6 @@ import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 import contactsReducer from './contacts/contacts-reducers';
 
-
-
 const rootReducer = combineReducers({
     contacts: contactsReducer
 });
