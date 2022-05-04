ea23bff771de16b9324f001a5d1bdab0404fce0b
Selim Fazylov <selim.fazylov1@gmail.com>
2022-05-04 14:35:28 +0300

    refactored with combinereducer

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,27 +1,34 @@
 import { createStore, combineReducers } from "redux";
 import { composeWithDevTools } from 'redux-devtools-extension';
 
-const initialState = {
-    items: JSON.parse(window.localStorage.getItem('contacts')) ?? [],
-    filter: ''
-}
-
-const reducer = (state = initialState, { type, payload }) => {
+const itemsReducer = (state = JSON.parse(window.localStorage.getItem('contacts')) ?? [], { type, payload }) => {
     switch (type) {
         case 'add':
-            return { ...state, items: [...state.items, payload] };
-        
-        case "filter":
-            return { ...state, filter: payload };
+            return [...state, payload];
         
         case 'deleteItem':
             const filteredList = list => (list.filter(e => e.id !== payload));
-            return { ...state, items: filteredList(state.items) };
+            return filteredList(state);
         
         default:
             return state;
     };
 };
-const rootReducer = combineReducers({ contacts: reducer });
-export const store = createStore(rootReducer, composeWithDevTools());
 
+const filterReducer = (state = '', { type, payload }) => {
+    switch (type) {
+        case "filter":
+            return payload;
+        
+        default:
+            return state;
+    };
+};
+
+const contactsReducer = combineReducers({
+    items: itemsReducer,
+    filter: filterReducer
+});
+
+const rootReducer = combineReducers({ contacts: contactsReducer });
+export const store = createStore(rootReducer, composeWithDevTools());
